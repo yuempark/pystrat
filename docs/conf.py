2f5be6f2d2fdaf6cecd0036223e25ee354dd50ad
Adrian Tasistro-Hart <adrian@tasistro-hart.com>
2025-02-25 13:50:23 -0800

    clarify where example data can be found

--- docs/conf.py
+++ docs/conf.py
@@ -138,7 +138,7 @@ release = version
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
-exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", ".virtual_documents/*" , "jupyter_execute", "**/jupyter_execute/*"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".venv", ".virtual_documents/*", "jupyter_execute", "**/jupyter_execute/*"]
 
 # The reST default role (used for this markup: `text`) to use for all documents.
 # default_role = None
